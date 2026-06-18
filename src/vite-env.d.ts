a26dfa61ba3519b5ef9fc8227f0dd8f28af3f029
Mohamed Mohsen Abdelhady <255518276+ApexBuildsHQ@users.noreply.github.com>
2026-06-18 19:30:23 +0300

    Delete src directory

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare interface ImportMeta {
-  readonly env: ImportMetaEnv;
-  glob<T = Record<string, unknown>>(pattern: string, options?: { eager?: boolean }): Record<string, T>;
-  globEager<T = Record<string, unknown>>(pattern: string): Record<string, T>;
-}
-
-declare interface ImportMetaEnv {}
